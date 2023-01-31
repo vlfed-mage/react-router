70ba7b1bd6a5807423aa1ddd00d899b6064741cb
Valdo <gkorchun@gmail.com>
2023-01-31 13:30:07 +0200

    Divided add into pages

---
+++ src/components/products/products.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import logo from '../../images/logo.svg'
+
+const Products = () => {
+    return (
+        <div className='product'>
+            <img src={ logo } alt='Ultimate burgers' className='logo'/>
+            Products
+        </div>
+    );
+};
+
+export default Products;
