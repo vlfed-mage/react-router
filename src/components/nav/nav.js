a994234fa11eda20cb92dcb24b53c582d940e62a
Valdo <gkorchun@gmail.com>
2023-01-31 14:42:41 +0200

    Navigation features

---
+++ src/components/nav/nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav className='nav' >
+            <NavLink // activeClassName and activeStyle have been removed from NavLinkProps
+                to='/'
+                className={ ({ isActive }) => isActive ? 'active' : null }
+                end >
+                Products
+            </NavLink>
+            <NavLink
+                to='/admin'
+                className={ ({ isActive }) => isActive ? 'active another-class' : null }
+                style={ ({ isActive }) => isActive ? { fontStyle: 'italic', fontWeight: 700 } : null } >
+                Admin
+            </NavLink>
+        </nav>
+    );
+};
+
+export default Nav;
