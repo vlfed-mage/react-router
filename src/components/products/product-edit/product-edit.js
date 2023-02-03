5af33639b472c05a5fc952831ad861e1b89189b5
Valdo <gkorchun@gmail.com>
2023-02-03 17:47:16 +0200

    Added form's actioins

--- src/components/products/product-edit/product-edit.js
+++ src/components/products/product-edit/product-edit.js
@@ -1,32 +1,53 @@
 import React from 'react';
+import { useState } from 'react';
 
 const ProductEdit = () => {
+    const [form, setForm] = useState({
+        id: '',
+        name: '',
+        price: '',
+        description: ''
+    });
+
+    const updateFields = (target) => {
+        const { name, value } = target;
+        setForm({
+            ...form,
+            [name]: name === 'price'
+                ? isNaN(parseInt(value)) ? '' : parseInt(value)
+                : value
+        })
+    }
+
+    const fields = ['id', 'name', 'price'];
+    const mapFields = fields.map((field) => {
+        return (
+            <input
+                key={ field }
+                type='text'
+                name={ field }
+                placeholder={ field.charAt(0).toUpperCase() + field.slice(1) }
+                className='product-edit-input'
+                onChange={ ({ target }) => updateFields(target) }
+                value={ form ? form[field] : '' }
+            />
+        );
+    });
+
     return (
-        <form className='product-edit'>
-            <input
-                type='text'
-                name='id'
-                placeholder='ID'
-                className='product-edit-input'
-            />
-            <input
-                type='text'
-                name='name'
-                placeholder='Name'
-                className='product-edit-input'
-            />
-            <input
-                type='text'
-                name='price'
-                placeholder='Price'
-                className='product-edit-input'
-            />
-            <textarea
-                name='description'
-                placeholder='Description'
-                className='product-edit-input product-edit-textarea'
-            />
-        </form>
+        <>
+            { JSON.stringify(form) }
+            <form className='product-edit'>
+                { mapFields }
+                <textarea
+                    name='description'
+                    placeholder='Description'
+                    className='product-edit-input product-edit-textarea'
+                    onChange={ ({ target }) => updateFields(target) }
+                    value={ form ? form.description: '' }
+                />
+            </form>
+        </>
     );
 };
 
